b3c10504c5d20f9eb421a488e8f6053bc7b28e43
Ashley Eglin <eglina@parliament.uk>
2015-08-20 16:49:11 +0100

    004: Started work on showing train information

---
+++ Dashboard.App/Dashboard.WebApp.UI/assets/js/src/controllers/TrainCtrl.js
@@ -0,0 +1,24 @@
+﻿angular.module('dashboardApp').controller('TrainCtrl', function($scope, TrainService, $timeout) {
+
+    var routes = [{ from: "KGX", to: "SVG" }, { from: "HUL", to: "KGX" }, { from: "CPM", to: "PAD" }];
+
+    var getDepartures = function (routeIndex, delay) {
+        $timeout(function () {
+            var status = TrainService.getDepartures(routes[routeIndex].from, routes[routeIndex].to);
+            status.then(function (result) {
+                $scope.route = routes[routeIndex].from + ' to ' + routes[routeIndex].to;
+                $scope.departures = result.TrainServices;
+
+                routeIndex++;
+
+                if (routeIndex === routes.length) {
+                    routeIndex = 0;
+                }
+
+                getDepartures(routeIndex, 10000);
+            });
+        }, delay, true);
+    };
+
+    getDepartures(0, 0);
+});
---
+++ Dashboard.App/Dashboard.WebApp.UI/assets/js/src/services/TrainService.js
@@ -0,0 +1,20 @@
+﻿angular.module('dashboardApp').factory('TrainService', function($http, ConfigService) {
+    var trainService = {};
+
+    trainService.getDepartures = function(from, to) {
+        if (navigator.onLine) {
+            return $http.get(ConfigService.getDashboardServiceBaseUrl() + 'trains/' + from + '/' + to, { cache: false, timeout: 5000 })
+                .then(function (response) {
+                    return response.data;
+                })
+                .catch(function (error) {
+                    console.log('Error getting train status.');
+                    return -1;
+                });
+        }
+
+        return null;
+    };
+
+    return trainService;
+});
