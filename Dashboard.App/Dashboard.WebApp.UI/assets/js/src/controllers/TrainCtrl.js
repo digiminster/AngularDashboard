6243ca50f8022901dabfb325b4ceeed8fdf138f2
Ashley Eglin <eglina@parliament.uk>
2015-08-21 10:20:19 +0100

    004: Shows two train routes next to each other

--- Dashboard.App/Dashboard.WebApp.UI/assets/js/src/controllers/TrainCtrl.js
+++ Dashboard.App/Dashboard.WebApp.UI/assets/js/src/controllers/TrainCtrl.js
@@ -1,24 +1,41 @@
-﻿angular.module('dashboardApp').controller('TrainCtrl', function($scope, TrainService, $timeout) {
+﻿angular.module('dashboardApp').controller('TrainCtrl', function($scope, TrainService, $interval) {
 
-    var routes = [{ from: "KGX", to: "SVG" }, { from: "HUL", to: "KGX" }, { from: "CPM", to: "PAD" }];
+    var routes = [{ from: "KGX", to: "SVG" }, { from: "HUL", to: "KGX" }, { from: "CPM", to: "PAD" }, { from: "HES", to: "BUR" }, { from: "IPS", to: "NRW" }];
 
-    var getDepartures = function (routeIndex, delay) {
-        $timeout(function () {
-            var status = TrainService.getDepartures(routes[routeIndex].from, routes[routeIndex].to);
-            status.then(function (result) {
-                $scope.route = routes[routeIndex].from + ' to ' + routes[routeIndex].to;
-                $scope.departures = result.TrainServices;
+    var getDepartures = function (routeIndexA, routeIndexB, delay) {
+        $interval(function () {
 
-                routeIndex++;
+            var statusA = TrainService.getDepartures(routes[routeIndexA].from, routes[routeIndexA].to);
+            statusA.then(function (result) {
+                $scope.routeA = routes[routeIndexA].from + ' to ' + routes[routeIndexA].to;
+                $scope.departuresA = result.TrainServices;
 
-                if (routeIndex === routes.length) {
-                    routeIndex = 0;
+                routeIndexA += 2;
+
+                if (routeIndexA >= routes.length) {
+                    routeIndexA = 0;
                 }
-
-                getDepartures(routeIndex, 10000);
             });
-        }, delay, true);
+
+            if (routeIndexB <= routes.length) {
+                var statusB = TrainService.getDepartures(routes[routeIndexB].from, routes[routeIndexB].to);
+                statusB.then(function(result) {
+                    $scope.routeB = routes[routeIndexB].from + ' to ' + routes[routeIndexB].to;
+                    $scope.departuresB = result.TrainServices;
+
+                    routeIndexB += 2;
+
+                    if (routeIndexB >= routes.length) {
+                        routeIndexB = 1;
+                    }
+                });
+            } else {
+                $scope.routeB = "";
+                $scope.departuresB = "";
+            }
+
+        }, delay, 0, true);
     };
 
-    getDepartures(0, 0);
+    getDepartures(0, 1, 10000);
 });
