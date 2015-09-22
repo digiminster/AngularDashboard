962488b07e7b35a23a1364e45402ec4faa0c8a82
Ashley Eglin <eglina@parliament.uk>
2015-09-22 14:10:54 +0100

    Refactored ConfigService.js

--- Dashboard.App/Dashboard.WebApp.UI/assets/js/src/services/ConfigService.js
+++ Dashboard.App/Dashboard.WebApp.UI/assets/js/src/services/ConfigService.js
@@ -1,35 +1,11 @@
 ﻿angular.module('dashboardApp').factory('ConfigService', function ($http) {
     var configService = {};
-    var configSettings = {
-        dashboardServiceBaseUrl: '',
-        tubeLines: '',
-        trainRoutes: '',
-        serverIps: '',
-        bugRefreshInterval: 0,
-        tubeRefreshInterval: 0,
-        tubeScrollInterval: 0,
-        trainRefreshInterval: 0,
-        trainScrollInterval: 0,
-        serverScrollInterval: 0,
-        serverRefreshInterval: 0,
-        bugsUrl: ''
-    };
+    var configSettings = {};
 
     configService.loadFile = function () {
         return $http.get('assets/config.json', { cache: false, timeout: 5000 })
                 .then(function (response) {
-                    configSettings.dashboardServiceBaseUrl = response.data.serviceBaseUrl;
-                    configSettings.tubeLines = response.data.tubeLines;
-                    configSettings.trainRoutes = response.data.trainRoutes;
-                    configSettings.serverIps = response.data.serverIps;
-                    configSettings.bugRefreshInterval = response.data.bugRefreshInterval;
-                    configSettings.tubeRefreshInterval = response.data.tubeRefreshInterval;
-                    configSettings.tubeScrollInterval = response.data.tubeScrollInterval;
-                    configSettings.trainRefreshInterval = response.data.trainRefreshInterval;
-                    configSettings.trainScrollInterval = response.data.trainScrollInterval;
-                    configSettings.serverScrollInterval = response.data.serverScrollInterval;
-                    configSettings.serverRefreshInterval = response.data.serverRefreshInterval;
-                    configSettings.bugsUrl = response.data.bugsUrl;
+                    configSettings = response;
             })
                 .catch(function (error) {
                     console.log("Could not load config file.");
@@ -37,51 +13,51 @@
     }
 
     configService.getDashboardServiceBaseUrl = function () {
-        return configSettings.dashboardServiceBaseUrl;
+        return configSettings.data.serviceBaseUrl;
     }
 
     configService.getTubeLines = function() {
-        return configSettings.tubeLines;
+        return configSettings.data.tubeLines;
     }
 
     configService.getTrainRoutes = function() {
-        return configSettings.trainRoutes;
+        return configSettings.data.trainRoutes;
     }
 
     configService.getServerIps = function () {
-        return configSettings.serverIps;
+        return configSettings.data.serverIps;
     }
 
     configService.getBugRefreshInterval = function() {
-        return configSettings.bugRefreshInterval;
+        return configSettings.data.bugRefreshInterval;
     }
 
     configService.getTubeRefreshInterval = function() {
-        return configSettings.tubeRefreshInterval;
+        return configSettings.data.tubeRefreshInterval;
     }
 
     configService.getTubeScrollInterval = function () {
-        return configSettings.tubeScrollInterval;
+        return configSettings.data.tubeScrollInterval;
     }
 
     configService.getTrainRefreshInterval = function () {
-        return configSettings.trainRefreshInterval;
+        return configSettings.data.trainRefreshInterval;
     }
 
     configService.getTrainScrollInterval = function () {
-        return configSettings.trainScrollInterval;
+        return configSettings.data.trainScrollInterval;
     }
 
     configService.getServerScrollInterval = function() {
-        return configSettings.serverScrollInterval;
+        return configSettings.data.serverScrollInterval;
     }
 
     configService.getServerRefreshInterval = function () {
-        return configSettings.serverRefreshInterval;
+        return configSettings.data.serverRefreshInterval;
     }
 
     configService.getBugsUrl = function () {
-        return configSettings.bugsUrl;
+        return configSettings.data.bugsUrl;
     }
 
     return configService;
