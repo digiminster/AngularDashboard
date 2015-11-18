61f1923b50a34bdc8665cf69f619d5886df83b90
stephenbolt <bolts@parliament.uk>
2015-11-18 17:06:09 +0000

    Adding a show/hide menu button for the top nav bar.

---
+++ Dashboard.App/Dashboard.WebApp.UI/assets/js/src/controllers/MenuCtrl.js
@@ -0,0 +1,15 @@
+﻿angular.module('dashboardApp').controller('MenuCtrl', function ($scope) {
+
+    $scope.showHideNavClass = "hidden";
+    $scope.showHideBodyClass = "";
+
+    $scope.showHideMenuButton = function () {
+        if ($scope.showHideNavClass === "hidden") {
+            $scope.showHideNavClass = "";
+            $scope.showHideBodyClass = "drop";
+        } else {
+            $scope.showHideNavClass = "hidden";
+            $scope.showHideBodyClass = "";
+        }
+    };
+});
