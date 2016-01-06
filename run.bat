3a5f697939289950993e8f68dcea3794b3b657c7
James McKay <code@jamesmckay.net>
2016-01-06 07:49:50 +0000

    Require a command line option to install dependencies

--- run.bat
+++ run.bat
@@ -1,3 +1,9 @@
-call npm install
+@echo off
+
+if "%1"=="install" (
+    call npm install
+    shift
+)
+
 SET mypath=%~dp0
 %mypath%node_modules\.bin\electron.cmd %mypath%Dashboard.App\Dashboard.WebApp.UI\main.js %*
