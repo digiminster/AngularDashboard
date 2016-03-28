ec31e65c73b529aa376a5fad3cd8db7ceb6dae7e
James McKay <code@jamesmckay.net>
2016-03-28 09:23:53 +0100

    Rename Dashboard.WebApp.UI to "app"

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 npm install
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/node_modules/.bin/electron $DIR/src/Dashboard.WebApp.UI/main.js "$@" & > /dev/null
+$DIR/node_modules/.bin/electron $DIR/src/app/main.js "$@" & > /dev/null
